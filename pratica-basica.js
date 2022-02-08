6a34065d41a3e256fc345e254f9fc29ac552f34c
Carlos Augusto <carlosfilho.canal@icloud.com>
2022-02-08 00:23:36 -0300

    modify pratica.js

--- pratica-basica.js
+++ pratica-basica.js
@@ -1,1 +1,11 @@
-sa
+/*O que eu quero? Uma lista com:
+NOME DO ALUNO, NOTA 1, NOTA 2 A MÉDIA
+E se o aluno foi aprovado ou reprovado.*/
+
+var nomes = ["carlos, joao, rafael"]
+var nota1 = [3,7.5,10]
+var nota2 = [7, 8.5, 1]
+
+function media(n1,n2){
+  return (n1 + n2) / 2
+}
