429f79e2719c9a6966410f8c5a1ab66b280a1f06
Carlos Augusto <carlosfilho.canal@icloud.com>
2022-02-08 22:21:34 -0300

    modify pratica basica

--- pratica-basica.js
+++ pratica-basica.js
@@ -2,10 +2,18 @@
 NOME DO ALUNO, NOTA 1, NOTA 2 A MÉDIA
 E se o aluno foi aprovado ou reprovado.*/
 
-var nomes = ["carlos, joao, rafael"]
+var nomes = ["carlos", "joao", "rafael"]
 var nota1 = [3,7.5,10]
 var nota2 = [7, 8.5, 1]
 
 function media(n1,n2){
   return (n1 + n2) / 2
+}
+
+for (var index in nomes){
+  console.log("Aluno(a) " + nomes[index] + 
+              " teve a sua primeira nota " +
+              nota1[index] +
+              " e a segunda " +
+              nota2[index])
 }
